93bc9bc7d1b2a6be34eaa24cbbd2a11fe0f297d5
purnimagurung <grg.puru@gmail.com>
2025-12-01 17:12:30 +0100

    Remove .env from git tracking

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_KEY = "98047e8270db769ebc85d1f85a649c2c";
+// const API_KEY = "98047e8270db769ebc85d1f85a649c2c";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
